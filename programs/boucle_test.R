81e77f78b9b40b8fdc2e764639c83287154a326c
Charles THÉVENIN <charles.thevenin@mnhn.fr>
2025-10-28 15:56:59 +0100

    bordel de here

--- programs/boucle_test.R
+++ programs/boucle_test.R
@@ -1,4 +1,4 @@
 library(here)
 library(quarto)
 
-quarto::quarto_render(input =  here::here("test.qmd"), output_file = "docs/test.html")
+quarto::quarto_render(input =  here::here("test.qmd"), output_file = here::here("docs", "test.html"))
